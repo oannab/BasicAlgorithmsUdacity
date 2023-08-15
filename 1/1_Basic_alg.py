dfaaa582ee637f46ceaca3bad7e11a81a90cad27
Oana B <35232908+oannab@users.noreply.github.com>
2023-08-15 14:38:01 +0300

    Update 1_Basic_alg.py
    edge test cases

--- 1/1_Basic_alg.py
+++ 1/1_Basic_alg.py
@@ -32,3 +32,17 @@ print ("Pass" if  (0 == sqrt(0)) else "Fail")
 print ("Pass" if  (4 == sqrt(16)) else "Fail")
 print ("Pass" if  (1 == sqrt(1)) else "Fail")
 print ("Pass" if  (5 == sqrt(27)) else "Fail")
+
+print("Pass" if  (-1 == sqrt(-1)) else "Fail") # negative input 
+
+print("Pass" if  (0 == sqrt(0)) else "Fail") # 0 input
+
+print("Pass" if  (1 == sqrt(1)) else "Fail") # 1 input
+
+print("Pass" if  (2 == sqrt(5)) else "Fail") # prime number 
+
+print("Pass" if  (3 == sqrt(9)) else "Fail") # prime number and exact square
+
+print("Pass" if  (4 == sqrt(17)) else "Fail") # prime number and non exact square
+
+print("Pass" if  (100 == sqrt(10002)) else "Fail") # large no and non exact square
